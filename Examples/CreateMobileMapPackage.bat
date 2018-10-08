c0dc84c4396953ed5574ee53892b8db686b9c735
Shaun Weston <shaun_weston@eagle.co.nz>
2018-10-08 15:57:50 +1300

    Adding new tools.

---
+++ Examples/CreateMobileMapPackage.bat
@@ -0,0 +1,11 @@
+REM ----- Create Mobile Map Package -----
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\Development\Python for ArcGIS Tools\ArcGIS Enterprise Toolkit\CreateMobileMapPackage.py"
+ "\\GISDATA\Data\Services\Streets.mapx" ^
+ "" ^
+ "https://organisation/arcgis" ^
+ "WairarapaCouncil" ^
+ "*****" ^
+ "*****" ^
+ "Streets" ^
+ "Streets basemap for the Wairarapa region of New Zealand." ^
+ "Streets, Service, Basemap, Wairarapa"
---
+++ Examples/CreateVectorTilePackage.bat
@@ -0,0 +1,11 @@
+REM ----- Create Vector Tile Package -----
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "\\GISDATA\Data\Tools & Scripts\ArcGIS Admin Toolkit\CreateVectorTilePackage.py" ^
+ "\\GISDATA\Data\Services\Streets.mapx" ^
+ "\\GISDATA\Data\Tools & Scripts\ArcGIS Admin Toolkit\NZTMTileScheme.xml" ^
+ "https://organisation/arcgis" ^
+ "Organisation" ^
+ "*****" ^
+ "377fe5a6ef814baeaf2e6e921daefa75" ^
+ "Streets" ^
+ "Streets basemap for the Wairarapa region of New Zealand." ^
+ "Streets, Service, Basemap, Wairarapa"
